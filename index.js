5f7266d1d0bc43629f91a19b3461dcaafaf93d56
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2016-02-01 22:12:04 +0100

    Working on npm-test-requirejs-deps

---
+++ index.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+var data = {};
+
+function readRequireJSModules(dirName, setResult, onError) {
+    var parentDir = dirName;
+
+    if (dirName && dirName !== 'node_modules') {
+        (function travelDir(dir) {
+            var directory = dir || dirName;
+            fs.readdir(directory, function(err, fileNames) {
+                if (err) {
+                    onError(err);
+                    return;
+                }
+
+                fileNames.forEach(function(fileName) {
+                    if (fileName.indexOf('.js') !== -1) {
+                        fs.readFile(directory + '/' + fileName, 'utf-8', function(err, content) {
+                            if (err) {
+                                onError(err);
+                                return;
+                            }
+                            setResult(fileName, content);
+                        });
+                    } else {
+                        if (fs.lstatSync(fileName).isDirectory()) {
+                            parentDir += '/' + fileName;
+                            travelDir(parentDir);
+                        } else {
+                            return;
+                        }
+                    }
+                });
+                directory = '';
+            });
+        }(dirName));
+    } else {
+        var errorMessage = 'Your directory name is empty';
+        setError(errorMessage);
+    }
+}
+
+function setResult(fileName, content) {
+    if (content.indexOf('define') !== -1) {
+        console.log(fileName);
+    }
+}
+
+function setError(err) {
+    console.log(err);
+}
+
+module.exports = function(dirName) {
+    var dir = dirName || '.';
+    readRequireJSModules(dir, setResult, setError);
+}
