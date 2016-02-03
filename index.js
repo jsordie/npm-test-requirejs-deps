74b10b61c23ac0a6c8e093d17f2f2f9e8b5328fe
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2016-02-03 21:22:52 +0100

    Working on index.js

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-var fs = require('fs');
-var path = require('path');
-var wrench = require('wrench');
-var chalk = require('chalk');
-var data = {};
+var fs = require('fs'),
+    path = require('path'),
+    wrench = require('wrench'),
+    chalk = require('chalk'),
+    requirejs = require('requirejs'),
+    //requirejsText = require('requirejs-text'),
+    foldersToRemove = null;
 
-function readRequireJSModules(dirName, setResult, onError) {
-    var files = wrench.readdirSyncRecursive(dirName);
+function removeFolders(file) {
+    var i = 0,
+        arrLength = foldersToRemove.length,
+        result;
+
+    for (i; i < arrLength; i++) {
+        if (file.indexOf(foldersToRemove[i]) === -1) {
+            result = file;
+        } else {
+            result = false;
+        }
+    }
+    return (result) ? result : false;
+}
+
+function returnJSfiles(file) {
+    return (file.indexOf('.json') === -1 && file.indexOf('.jsq') === -1 && file.indexOf('node_modules') === -1 && file.indexOf('.git') === -1 && file.indexOf('.js') !== -1) ? file : false;
+}
+
+function readRequireJSModules(dirName, arr, setResult, onError) {
+    var files = wrench.readdirSyncRecursive(dirName).filter(returnJSfiles).filter(removeFolders);
 
     if (dirName) {
         files.forEach(function(file) {
-            if (file.indexOf('node_modules') === -1 && file.indexOf('.git') === -1 && file.indexOf('coverage') === -1 && file.indexOf('.js') !== -1) {
+            if (file.indexOf('.js') !== -1) {
                 fs.readFile(dirName + '/' + file, 'utf-8', function(err, content) {
                     if (err) {
                         onError(err);
@@ -25,16 +46,17 @@ function readRequireJSModules(dirName, setResult, onError) {
 }
 
 function setResult(fileName, content) {
-    if (content.indexOf('define') !== -1) {
+    if (content.indexOf('define') !== -1 || content.indexOf('require') !== -1) {
         console.log(fileName);
     }
 }
 
 function setError(err) {
-    console.log(err);
+    //console.log(err);
 }
 
-module.exports = function(dirName) {
+module.exports = function(dirName, arr) {
     var dir = dirName || path.resolve(__dirname);
-    readRequireJSModules(dir, setResult, setError);
+    foldersToRemove = arr;
+    readRequireJSModules(dir, arr, setResult, setError);
 }
