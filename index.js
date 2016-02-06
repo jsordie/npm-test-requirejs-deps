fda72d908da2e0af90daffaa7fd78f74be39d25b
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2016-02-06 20:22:21 +0100

    index.js improved

--- index.js
+++ index.js
@@ -6,25 +6,38 @@ var fs = require('fs'),
     foldersToExclude,
     configFile;
 
+/**
+ * excludeFolders
+ * method to exclude directories or files
+ * @param  {string} file
+ * @return {string}
+ */
 function excludeFolders(file) {
-    var i = 0,
-        arrLength = foldersToExclude.length,
-        result;
+    var result = false;
 
-    for (i; i < arrLength; i++) {
-        if (file.indexOf(foldersToExclude[i]) === -1) {
-            result = file;
-        } else {
-            result = false;
-        }
-    }
+    foldersToExclude.map(function(f) {
+        (file.indexOf(f) === -1 && !result) ? result = file: result = false;
+    });
     return (result) ? result : false;
 }
 
+/**
+ * returnJSfiles
+ * method that returns only js files
+ * @param  {string} file
+ * @return {string}
+ */
 function returnJSfiles(file) {
     return (file.indexOf('.json') === -1 && file.indexOf('.jsq') === -1 && file.indexOf('node_modules') === -1 && file.indexOf('.git') === -1 && file.indexOf('.js') !== -1) ? file : false;
 }
 
+/**
+ * readRequireJSModules
+ * @param  {string} dirName   [folfer name]
+ * @param  {array}  arr       [array of folders or files to exclude]
+ * @param  {object} setResult [setResult method]
+ * @param  {object} onError   [setError method]
+ */
 function readRequireJSModules(dirName, arr, setResult, onError) {
     var files = wrench.readdirSyncRecursive(dirName).filter(returnJSfiles).filter(excludeFolders);
 
@@ -43,33 +56,56 @@ function readRequireJSModules(dirName, arr, setResult, onError) {
     }
 }
 
+/**
+ * generateDependo
+ * Show dependo result
+ * @param  {string} file
+ */
 function generateDependo(file) {
-    var dependo = new Dependo(file, {
-        format: 'amd',
-        requireConfig: configFile,
-        transform: function(dep) {
-            for (d in dep) {
-                if (dep.hasOwnProperty(d)) {
-                    if (dep[d].length > 0) {
-                        chalk.blue(console.log('--> ' + d + ': ' + dep[d]));
+    if (file) {
+        var dependo = new Dependo(file, {
+            format: 'amd',
+            requireConfig: configFile,
+            transform: function(dep) {
+                for (d in dep) {
+                    if (dep.hasOwnProperty(d)) {
+                        if (dep[d].length > 0) {
+                            chalk.blue(console.log('--> ' + d + ': ' + dep[d]));
+                        }
                     }
                 }
             }
-        }
-    });
-    dependo.generateHtml();
+        });
+        dependo.generateHtml();
+    }
 }
 
+/**
+ * setResult
+ * Check that the module is created as a RequireJS module
+ * @param {string} fileName
+ * @param {string} content  [file content]
+ */
 function setResult(fileName, content) {
     if (content.indexOf('define(') !== -1 || content.indexOf('require(') !== -1) {
         generateDependo(fileName);
     }
 }
 
+/**
+ * setError
+ * Show the error in console
+ * @param {string} err [error text]
+ */
 function setError(err) {
     chalk.red(console.log(err));
 }
 
+/**
+ * @param  {string} dirName [folfer name]
+ * @param  {array}  arr     [array of folders or files to exclude]
+ * @param  {string} conf    [requirejs config file]
+ */
 module.exports = function(dirName, arr, conf) {
     var dir = dirName || path.resolve(__dirname);
     foldersToExclude = arr;
