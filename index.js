9c805bd848718f54b467b4a3ba46f5f3b785aee2
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2016-02-07 21:17:49 +0100

    index.js improved

--- index.js
+++ index.js
@@ -3,8 +3,104 @@ var fs = require('fs'),
     wrench = require('wrench'),
     chalk = require('chalk'),
     Dependo = require('dependo'),
+    mkdirp = require('mkdirp'),
     foldersToExclude,
     configFile;
+
+/**
+ * readRequireJSModules
+ * @param  {String} dirName     [folfer name]
+ * @param  {Array}  arr         [array of folders or files to exclude]
+ * @param  {Function} setResult [setResult method]
+ * @param  {Function} setError  [setError method]
+ * @param  {Function} generateDependo [generateDependo method]
+ */
+function readRequireJSModules(dirName, arr, setResult, setError, generateDependo) {
+    var files = wrench.readdirSyncRecursive(dirName).filter(returnJSfiles).filter(excludeFolders);
+
+    if (dirName) {
+        files.forEach(function(file) {
+            if (file.indexOf('.js') !== -1) {
+                fs.readFile(dirName + '/' + file, 'utf-8', function(err, content) {
+                    if (err) {
+                        setError(err);
+                    }
+                    setResult(file, content, setError);
+                });
+            }
+        });
+        generateDependo(files);
+    }
+}
+
+/**
+ * generateDependo
+ * Show dependo result
+ * @param  {String} file
+ */
+function generateDependo(files) {
+    var html, dependo;
+    if (files.length > 0) {
+        dependo = new Dependo(files, {
+            format: 'amd',
+            requireConfig: configFile,
+            title: 'Project Depenedencies',
+            exclude: '^node_modules',
+            transform: function(dep) {
+                for (d in dep) {
+                    if (dep[d] && dep[d].length > 0) {
+                        chalk.blue(console.log('--> ' + d + ': ' + dep[d]));
+                    }
+                }
+                return dep;
+            }
+        });
+    }
+    html = dependo.generateHtml();
+    exportHtml(html, setError);
+}
+
+/**
+ * exportHtml
+ * Generates an HTML file in the directory ./requirejsDeps
+ * @param  {String} content [HTML content]
+ * @param  {Function} setError [setError method]
+ */
+function exportHtml(content, setError) {
+    mkdirp('./requirejsDeps', function(err) {
+        if (err) {
+            setError(err);
+        } else {
+            fs.writeFile('./requirejsDeps/index.html', content, function(err) {
+                if (err) setError(err);
+            });
+        }
+    });
+}
+
+/**
+ * setResult
+ * Check that the module is created as a RequireJS module
+ * @param {String} fileName   [file name]
+ * @param {String} content    [file content]
+ * @param {Function} callback [generateDependo method]
+ * @param {Function} setError [setError method]
+ */
+function setResult(fileName, content, setError) {
+    var depArr = {};
+    if (content.indexOf('define(') !== -1 || content.indexOf('require(') !== -1) {
+        depArr[fileName] = content;
+    }
+}
+
+/**
+ * setError
+ * Show the error in console
+ * @param {String} err [error text]
+ */
+function setError(err) {
+    chalk.red(console.log(err));
+}
 
 /**
  * excludeFolders
@@ -27,86 +123,6 @@ function returnJSfiles(file) {
 }
 
 /**
- * readRequireJSModules
- * @param  {String} dirName     [folfer name]
- * @param  {Array}  arr         [array of folders or files to exclude]
- * @param  {Function} setResult [setResult method]
- * @param  {Function} setError  [setError method]
- */
-function readRequireJSModules(dirName, arr, setResult, setError) {
-    var files = wrench.readdirSyncRecursive(dirName).filter(returnJSfiles).filter(excludeFolders);
-
-    if (dirName) {
-        files.forEach(function(file) {
-            if (file.indexOf('.js') !== -1) {
-                fs.readFile(dirName + '/' + file, 'utf-8', function(err, content) {
-                    if (err) {
-                        setError(err);
-                        return;
-                    }
-                    setResult(file, content, generateDependo);
-                });
-            }
-        });
-    }
-}
-
-/**
- * generateDependo
- * Show dependo result
- * @param  {String} file
- */
-function generateDependo(file) {
-    if (file) {
-        var dependo = new Dependo(file, {
-                format: 'amd',
-                requireConfig: configFile,
-                title: 'Project Depenedencies',
-                transform: function(dep) {
-                    for (d in dep) {
-                        if (dep[d] && dep[d].length > 0) {
-                            chalk.blue(console.log('--> ' + d + ': ' + dep[d]));
-                        }
-                    }
-                }
-            }),
-            html = dependo.generateHtml();
-        exportHtml(html);
-    }
-}
-
-/**
- * exportHtml
- * Generates an HTML file in the directory ./dependenciesHTML
- * @param  {String} content [HTML content]
- */
-function exportHtml(content) {
-    // TODO: Pending...
-}
-
-/**
- * setResult
- * Check that the module is created as a RequireJS module
- * @param {String} fileName   [file name]
- * @param {String} content    [file content]
- * @param {Function} callback [generateDependo method]
- */
-function setResult(fileName, content, callback) {
-    if (content.indexOf('define(') !== -1 || content.indexOf('require(') !== -1) {
-        callback(fileName);
-    }
-}
-
-/**
- * setError
- * Show the error in console
- * @param {String} err [error text]
- */
-function setError(err) {
-    chalk.red(console.log(err));
-}
-
-/**
  * module exports function
  * @param  {String} dirName [folfer name]
  * @param  {Array}  arr     [array of folders or files to exclude]
@@ -116,5 +132,5 @@ module.exports = function(dirName, arr, conf) {
     var dir = dirName || path.resolve(__dirname);
     foldersToExclude = arr || [];
     configFile = conf || '';
-    readRequireJSModules(dir, arr, setResult, setError);
+    readRequireJSModules(dir, arr, setResult, setError, generateDependo);
 }
